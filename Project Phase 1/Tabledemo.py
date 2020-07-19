f01da7b4a483b2804cb84ec7e67f6729dedd245d
phuongstudent <pddang@ucalgary.ca>
2020-07-19 14:19:01 -0600

    Don Update to the Tabledemo.py

--- Project Phase 1/Tabledemo.py
+++ Project Phase 1/Tabledemo.py
@@ -2,13 +2,118 @@ from tkintertable import TableCanvas, TableModel
 from tkinter import *
 import random
 from collections import OrderedDict
+import pymongo
+import dns
+import matplotlib.pyplot as plt
+import numpy as np
 
+from pymongo import MongoClient
 
 # Example of how to create and import data as dictionary
 # data = {'rec1': {'col1': 99.88, 'col2': 108.79, 'label': 'rec1'},
 #        'rec2': {'col1': 99.88, 'col2': 321.79, 'label': 'rec3'},
 #        'rec3': {'col1': 29.88, 'col2': 408.79, 'label': 'rec2'}
 #        }
+
+
+
+# Connect to MongoDB
+cluster = MongoClient("mongodb+srv://sarim:1234@cluster0.azpgb.mongodb.net/ENSF592?retryWrites=true&w=majority")
+
+# Create variable to access database
+db = cluster["ENSF592"]
+
+# Create variable to access database collections and use .find() to return as a cursor object
+traffic_incidents = db["Traffic_Incidents"].find()
+
+
+
+# create empty dictionaries to be populated with data based on year
+incidents_2016 = dict()
+# buffer that hold the second dict in the dict of dict for the table
+incidents_2016_buffer = dict()
+
+incidents_2017 = dict()
+# buffer that hold the second dict in the dict of dict for the table
+incidents_2017_buffer = dict()
+
+incidents_2018 = dict()
+# buffer that hold the second dict in the dict of dict for the table
+incidents_2018_buffer = dict()
+
+# dict that hold the total count of each incident info
+incident_2016_sum = dict()
+incident_2017_sum = dict()
+incident_2018_sum = dict()
+# Hold a copy of "Traffic_Incidents" , to be used to get the total count of each incident info
+traffic_incidents_Copy = db["Traffic_Incidents"].find()
+
+# Loop to get the total count of each incident info
+for element in traffic_incidents_Copy:
+    if element["id"][:4] == "2016":  # 2016 data
+            incident_2016_sum[element["INCIDENT INFO"]] = incident_2016_sum.get(element["INCIDENT INFO"], 0) + 1
+    elif element["id"][:4] == "2017":  # 2017 data
+            incident_2017_sum[element["INCIDENT INFO"]] = incident_2017_sum.get(element["INCIDENT INFO"], 0) + 1
+    elif element["id"][:4] == "2018":  # 2018 data
+            incident_2018_sum[element["INCIDENT INFO"]] = incident_2018_sum.get(element["INCIDENT INFO"], 0) + 1
+    else:
+        continue
+
+# iterate through data collection and add incident information to corresponding dict based on year
+for element in traffic_incidents:
+    # this variable is only use to skip the fist iteration
+    count = 0
+    for key,value in element.items():
+        #skip the first iteration, since the first iteration is just "_id" in the collection
+        if count != 0:
+            if element["id"][:4] == "2016":  # 2016 data
+                if key == 'INCIDENT INFO':
+                    #Assign the total count for each incident
+                    incidents_2016_buffer["Sum of Incidents"] = incident_2016_sum[value]
+                    incidents_2016_buffer[key] = str(value)
+                elif key != 'id':
+                    # assigning the column header with the row information
+                    incidents_2016_buffer[key] = str(value)
+                # if it is the "ID" column, use this column as the key for "incidents_2016"
+                else:
+                    # assigning the column header with the row information
+                    incidents_2016_buffer[key] = str(value)
+                    # Store the dict into the dict of dict table
+                    incidents_2016[value] = incidents_2016_buffer.copy()
+
+            if element["id"][:4] == "2017":  # 2017 data
+                if key == 'INCIDENT INFO':
+                    # Assign the total count for each incident
+                    incidents_2017_buffer["Sum of Incidents"] = incident_2017_sum[value]
+                    incidents_2017_buffer[key] = str(value)
+                elif key != "id":
+                    # assigning the column header with the row information
+                    incidents_2017_buffer[key] = str(value)
+                # if it is the "ID" column, use this column as the key for "incidents_2016"
+                else:
+                    # assigning the column header with the row information
+                    incidents_2017_buffer[key] = str(value)
+                    # Store the dict into the dict of dict table
+                    incidents_2017[value] = incidents_2017_buffer.copy()
+            if element["id"][:4] == "2018":  # 2018 data
+                if key == 'INCIDENT INFO':
+                    # Assign the total count for each incident
+                    incidents_2018_buffer["Sum of Incidents"] = incident_2018_sum[value]
+                    incidents_2018_buffer[key] = str(value)
+                elif key != "id":
+                    # assigning the column header with the row information
+                    incidents_2018_buffer[key] = str(value)
+                # if it is the "ID" column, use this column as the key for "incidents_2016"
+                else:
+                    # assigning the column header with the row information
+                    incidents_2018_buffer[key] = str(value)
+                    # Store the dict into the dict of dict table
+                    incidents_2018[value] = incidents_2018_buffer.copy()
+
+        # this variable is only use to skip the fist iteration
+        count = count + 1
+
+
 
 
 # Class in charge of creating, displaying and manipulating table
@@ -30,10 +135,10 @@ class TestApp(Frame):
         # f.pack(fill=tk.Y,expand=-1,side = tk.LEFT)
 
         # Create/Format table
-        table = TableCanvas(f, cellwidth=60, cellbackgr='white', thefont=('Arial',12),rowheight=25, rowheaderwidth=30, rowselectedcolor='yellow', editable=True)
+        table = TableCanvas(f, cellwidth=60, data = incidents_2016, cellbackgr='white', thefont=('Arial',12),rowheight=25, rowheaderwidth=30, rowselectedcolor='yellow', editable=True)
 
         #Import table from csv
-        table.importCSV('2017_Traffic_Volume_Flow.csv')
+        #table.importCSV('2017_Traffic_Volume_Flow.csv')
 
         """if importing table as dictionary, use this: data is of type dictionary
         """
@@ -43,6 +148,9 @@ class TestApp(Frame):
 
         print (table.model.columnNames)
         table.show()
+        # sort the first column from highest to lowest (the sum of incident column)
+        table.sortTable(reverse=1)
+
         return
 
 import tkinter as tk
@@ -127,3 +235,20 @@ app.grid(row=0, column=1, sticky="nsew")
 
 # Keep listening for events
 window.mainloop()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

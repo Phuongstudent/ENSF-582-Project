249938521bb769ff4c9777fe0efd0da45b9c2c5a
phuongstudent <pddang@ucalgary.ca>
2020-07-19 23:38:08 -0600

    Updaded Tabledemo.py, read, sort buttons now work

--- Project Phase 1/Tabledemo.py
+++ Project Phase 1/Tabledemo.py
@@ -35,15 +35,23 @@ frame_b = tk.Frame(master = frame_a)
 frame_c = tk.Frame(master = frame_a)
 
 # Right frame initially
-frame_d = tk.Frame(window
-app.grid(row=0, column=1, sticky="nsew")                )
+frame_d = tk.Frame()
+# "Status" display
+initial_display = tk.Label(master = frame_d, text="Please choose the 'Type' and 'Year' of the report", foreground = "red", background = "white", width = 100, height = 50)
+initial_display.pack(side = tk.TOP)
+frame_d.grid(row=0, column=1, sticky="nsew")
+
+
+
+
+
 
 # Label for "Type" button
 button_Type_Label = tk.Label(master = frame_b,text="Type", width = 25, height = 5)
 # Position from left to right
 button_Type_Label.pack(side = tk.LEFT)
 # Combobox for "Type" button
-button_Type = ttk.Combobox(master = frame_b ,text = "Type", width = 25, height = 5)
+button_Type = ttk.Combobox(master = frame_b ,text = "Type", width = 25, height = 5,  state="readonly")
 button_Type['values'] = ('Accident', 'Traffic Volume')
 # Position from left to right
 button_Type.pack(side = tk.LEFT)
@@ -54,7 +62,7 @@ frame_b.grid(row=0, column=0, sticky="ew", padx=10)
 button_Year_Label = tk.Label(master = frame_c,text="Year", width = 25, height = 5)
 button_Year_Label.pack(side = tk.LEFT)
 # Combobox for "Year" button
-button_Year = ttk.Combobox(master = frame_c,text = "Year", width = 25, height = 5)
+button_Year = ttk.Combobox(master = frame_c,text = "Year", width = 25, height = 5, state="readonly")
 button_Year['values'] = ('2016', '2017', '2018')
 # Position from left to right
 button_Year.pack(side = tk.LEFT)
@@ -95,11 +103,101 @@ status_display.grid(row=7, column=0, sticky="ew", padx=10)
 frame_a.grid(row=0, column=0, sticky="ns")
 
 # initialize table
-app = App(window)
-app.importData(trafficIncident.incidents_2016)
-# add table to first row, second column, extend everywhere
-app.grid(row=0, column=1, sticky="nsew")
-app.sortData(1)
+
+# Incident 2016
+table_incident2016 = App(window)
+
+# Incident 2017
+table_incident2017 = App(window)
+# table_incident2017.importData(trafficIncident.incidents_2017)
+# Incident 2018
+table_incident2018 = App(window)
+# table_incident2018.importData(trafficIncident.incidents_2018)
+
+#Volume 2016
+# table_volume2016 = App(window)
+# table_volume2016.importData(trafficIncident.incidents_2016)
+
+#flag variables
+type_check_accident = False
+type_check_trafficvolume = False
+year_check2016 = False
+year_check2017 = False
+year_check2018 = False
+
+
+# Callbacks function for Type button
+def callback_button_type(event):
+    global type_check_accident
+    global type_check_trafficvolume
+    type_check_accident = False
+    type_check_trafficvolume = False
+    if button_Type.get() == "Accident":
+        type_check_accident = True
+    elif button_Type.get() == "Traffic Volume":
+        type_check_trafficvolume = True
+# Event listener for when a combo is selected
+button_Type.bind("<<ComboboxSelected>>", callback_button_type)
+
+
+# Callbacks function for Year Button
+def callback_button_Year(event):
+    global year_check2016
+    global year_check2017
+    global year_check2018
+    year_check2016 = False
+    year_check2017 = False
+    year_check2018 = False
+    if button_Year.get() == "2016":
+        year_check2016 = True
+    elif button_Year.get() == "2017":
+        year_check2017 = True
+    elif button_Year.get() == "2018":
+        year_check2018 = True
+# Event listener for when a combo is selected
+button_Year.bind("<<ComboboxSelected>>", callback_button_Year)
+
+
+# Callbacks function for Read Button
+def callback_button_Read(event):
+    if type_check_accident==True and year_check2016==True:
+        table_incident2017.grid_forget()
+        table_incident2018.grid_forget()
+        table_incident2016.importData(trafficIncident.incidents_2016)
+    elif type_check_accident==True and year_check2017==True:
+        table_incident2016.grid_forget()
+        table_incident2018.grid_forget()
+        table_incident2017.importData(trafficIncident.incidents_2017)
+    elif type_check_accident==True and year_check2018==True:
+        table_incident2017.grid_forget()
+        table_incident2016.grid_forget()
+        table_incident2018.importData(trafficIncident.incidents_2018)
+# Event listener for when the button is clicked
+button_Read.bind("<Button-1>", callback_button_Read)
+
+
+# Callbacks function for Sort Button
+def callback_button_Sort(event):
+    table_incident2016.sortData(1)
+    table_incident2017.sortData(1)
+    table_incident2018.sortData(1)
+# Event listener for when the button is clicked
+button_Sort.bind("<Button-1>", callback_button_Sort)
+
+
+# to be implemented for analysis button
+def callback_button_Analysis(event):
+    print("New Element Selected")
+button_Analysis.bind("<Button-1>", callback_button_Analysis)
+
+# to be implemented for map button
+def callback_button_Map(event):
+    print("New Element Selected")
+button_Map.bind("<Button-1>", callback_button_Map)
+
+
+
+
 
 # Keep listening for events
 window.mainloop()
--- Project Phase 1/traffic_volume.py
+++ Project Phase 1/traffic_volume.py
@@ -13,6 +13,16 @@ class TrafficVolume:
     total_volume_2017 = 0
     total_volume_2018 = 0
 
+    # Create empty dictionaries to be populated with data depending on year
+    volume_2016 = dict()
+    volume_2017 = dict()
+    volume_2018 = dict()
+
+    # Buffer that holds second dict in the dict of a dict to create table
+    volume_2016_buffer = dict()
+    volume_2017_buffer = dict()
+    volume_2018_buffer = dict()
+
     def __init__(self):
 
         self.cluster = MongoClient("mongodb+srv://sarim:1234@cluster0.azpgb.mongodb.net/ENSF592?retryWrites=true&w"
@@ -33,6 +43,60 @@ class TrafficVolume:
 
         for result in self.traffic_volume_2018.find():
             self.total_volume_2018 += result["VOLUME"]
+
+    def create_volume_table_dict(self):
+        # iterate through data collection and add incident information to corresponding dict based on year
+        for element in self.traffic_volume_2016:
+            # this variable is only use to skip the fist iteration
+            count = 0
+            for key, value in element.items():
+                # skip the first iteration, since the first iteration is just "_id" in the collection
+                if count != 0:
+                    if key == 'INCIDENT INFO':
+                        # Assign the total count for each incident
+                        self.incidents_2016_buffer["Sum of Incidents"] = self.incident_2016_sum[value]
+                        self.incidents_2016_buffer[key] = str(value)
+                    elif key != 'id':
+                        # assigning the column header with the row information
+                        self.incidents_2016_buffer[key] = str(value)
+                    # if it is the "ID" column, use this column as the key for "incidents_2016"
+                    else:
+                        # assigning the column header with the row information
+                        self.incidents_2016_buffer[key] = str(value)
+                        # Store the dict into the dict of dict table
+                        self.incidents_2016[value] = self.incidents_2016_buffer.copy()
+
+                    if element["id"][:4] == "2017":  # 2017 data
+                        if key == 'INCIDENT INFO':
+                            # Assign the total count for each incident
+                            self.incidents_2017_buffer["Sum of Incidents"] = self.incident_2017_sum[value]
+                            self.incidents_2017_buffer[key] = str(value)
+                        elif key != "id":
+                            # assigning the column header with the row information
+                            self.incidents_2017_buffer[key] = str(value)
+                        # if it is the "ID" column, use this column as the key for "incidents_2016"
+                        else:
+                            # assigning the column header with the row information
+                            self.incidents_2017_buffer[key] = str(value)
+                            # Store the dict into the dict of dict table
+                            self.incidents_2017[value] = self.incidents_2017_buffer.copy()
+                    if element["id"][:4] == "2018":  # 2018 data
+                        if key == 'INCIDENT INFO':
+                            # Assign the total count for each incident
+                            self.incidents_2018_buffer["Sum of Incidents"] = self.incident_2018_sum[value]
+                            self.incidents_2018_buffer[key] = str(value)
+                        elif key != "id":
+                            # assigning the column header with the row information
+                            self.incidents_2018_buffer[key] = str(value)
+                        # if it is the "ID" column, use this column as the key for "incidents_2016"
+                        else:
+                            # assigning the column header with the row information
+                            self.incidents_2018_buffer[key] = str(value)
+                            # Store the dict into the dict of dict table
+                            self.incidents_2018[value] = self.incidents_2018_buffer.copy()
+
+                # this variable is only use to skip the fist iteration
+                count = count + 1
 
     def create_volume_graph(self):
         # Test creation of line graph
